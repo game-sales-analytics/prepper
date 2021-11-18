cd298ee8fa2057f711ec8223c1658df31356791a
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-18 08:26:13 +0330

    fix: add specific invalid sales property reason to invalid entries

--- main.py
+++ main.py
@@ -48,16 +48,34 @@ def is_valid_sales(s: str) -> bool:
 
 
 invalids = list()
+
+
 def is_valid_row(row: dict) -> bool:
     if not is_valid_year(row["year"]):
-        invalids.append(dict(['reason', f"invalid year of {row['year']}", ['entry', row]))
+        invalids.append(
+            dict([
+                ['reason', f"invalid year of {row['year']}"],
+                ['entry', row],
+            ])
+        )
         return False
     if not is_valid_rank(row["rank"]):
-        invalids.append(dict(['reason', f"invalid rank of {row['rank']}", ['entry', row]))
+        invalids.append(
+            dict([
+                ['reason', f"invalid rank of {row['rank']}"],
+                ['entry', row],
+            ])
+        )
         return False
-    if not all(map(is_valid_sales, map(lambda k: row[k], ['na_sales', 'eu_sales', 'jp_sales', 'other_sales', 'global_sales']))):
-        invalids.append(dict(['reason', f"invalid sales value", ['entry', row]))
-        return False
+    for k in ['na_sales', 'eu_sales', 'jp_sales', 'other_sales', 'global_sales']:
+        if not is_valid_sales(row[k]):
+            invalids.append(
+                dict([
+                    ['reason', f"invalid {k} value of {row[k]}"],
+                    ['entry', row],
+                ])
+            )
+            return False
 
     return True
 
@@ -100,7 +118,6 @@ def jsonify(dataset: list) -> str:
     )
 
 
-
 l = list()
 with open("./dataset.csv", "r+") as datafile:
     dictReader = csv.DictReader(
@@ -109,7 +126,7 @@ with open("./dataset.csv", "r+") as datafile:
         quotechar='"',
     )
     for row in dictReader:
-        l.append(d.copy())
+        l.append(dict())
         for key in row:
             l[len(l) - 1][key] = row[key]
 
@@ -118,6 +135,7 @@ data_json = jsonify(parse_data(cleanup_data(l)))
 
 with open("./clean-dataset.json", "w+") as output:
     output.write(data_json)
+
 
 with open("./invalids.json", "w+") as output:
     output.write(jsonify(invalids))
